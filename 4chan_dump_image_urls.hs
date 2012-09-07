baf18eeb6d28cdc82391aa55a9cd7103810d9179
Lyndon Maydwell <maydwell@gmail.com>
2012-09-07 10:59:51 +0800

    Fixing executable

--- 4chan_dump_image_urls.hs
+++ 4chan_dump_image_urls.hs
@@ -10,18 +10,17 @@ import Data.Maybe
 -- Main:
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of [b,t] -> threadP b t >>= return . maybe [] (mapMaybe image_url) >>= mapM_ putStrLn
-               _     -> putStrLn "Usage: 4chan_dump_image_urls <board> <thread>"
+main = do args <- getArgs
+          case args of [b,t] -> threadP b t >>= return . maybe [] (mapMaybe (image_url b)) >>= mapM_ putStrLn
+                       _     -> putStrLn "Usage: 4chan_dump_image_urls <board> <thread>"
 
--- Helpers:
-
-images :: IO [String]
-images = threadP "b" "423596740" >>= return . maybe [] (mapMaybe image_url)
-
-image_url :: Post -> Maybe String
-image_url p = mand [Just "http://images.4chan.org/b/src/", fmap show $ post_tim p, post_ext p]
+image_url :: String -> Post -> Maybe String
+image_url b p = mand [ Just "http://images.4chan.org/"
+                     , Just b
+                     , Just "/src/"
+                     , fmap show $ post_tim p
+                     , post_ext p
+                     ]
 
 mand :: Monoid a => [Maybe a] -> Maybe a
 mand = Prelude.foldl (liftM2 mappend) (Just mempty)
